2a5c7fda0d8bb9086cc01c323acc3bd474ea62b7
danilabovetti010-dotcom <danila.bovetti.010@alu.tecnica29de6.edu.ar>
2026-03-12 11:46:47 -0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,10 @@
+#Escribe un programa que lea un número entero y determine si es positivo, negativo o cero.
+
+numero = int (input ("ingresa un numero:"))
+
+if (numero>0):
+    print ("Ingreso de un numero positivo")
+elif (numero<0):
+    print ("Ingreso de un numero negativo")
+else:
+    print ("ingreso numero 0")
