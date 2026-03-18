e575d922518b09ccb469540bab8653fc90368ed0
danilabovetti010-dotcom <danila.bovetti.010@alu.tecnica29de6.edu.ar>
2026-03-18 10:45:43 -0300

    Add files via upload

---
+++ 1c.py
@@ -0,0 +1,8 @@
+#Escribe un programa que pida al usuario que ingrese su edad y determine si es mayor de edad (18 años o más) o menor de edad.
+
+edad=int(input("ingresa tu edad: "))
+
+if edad >= 18:
+    print("sos mayor de edad")
+else:
+    print ("sos menor de edad")
