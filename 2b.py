5ddeadf783d857ccda8f1ddbeae07743d75113ec
danilabovetti010-dotcom <danila.bovetti.010@alu.tecnica29de6.edu.ar>
2026-03-18 11:04:52 -0300

    Add files via upload

---
+++ 2b.py
@@ -0,0 +1,6 @@
+#Escribe un programa que calcule la suma de los primeros 100 números naturales (es decir, 1 + 2 + 3 + ... + 100).
+
+suma = 0
+for i in range(1, 101):
+    suma += i
+print("La suma de los primeros 100 numeros naturales es:", suma)
